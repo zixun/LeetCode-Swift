44168d991123a082ca3f8b68ba9bb28457ca7dad
zixun <chenyl.exe@gmail.com>
2024-10-04 16:50:17 +0800

    finish 1670

--- LeetCode-Swift/LeetCode-Swift/1670_DesignFrontMiddleBackQueue.swift
+++ LeetCode-Swift/LeetCode-Swift/1670_DesignFrontMiddleBackQueue.swift
@@ -19,11 +19,20 @@ class Solution_1670_DesignFrontMiddleBackQueue {
         }
     }
     
+    enum ChangePosition {
+        case front
+        case middle
+        case back
+    }
+    enum ChangeType {
+        case push
+        case pop
+    }
+    
     private var head = Node(val: -1)
     private var foot = Node(val: -1)
     private var midd: Node?
     private var count = 0
-    private var mid_position = -1
     
     init() {
         self.head.next = self.foot
@@ -32,16 +41,12 @@ class Solution_1670_DesignFrontMiddleBackQueue {
     
     func pushFront(_ val: Int) {
         let node = Node(val: val)
-        
         self.insert(node, before: self.head.next)
-        self.count = self.count + 1
-        self.updateMidd()
+        self.updateMidd(type: .push, position: .front)
     }
     
     func pushMiddle(_ val: Int) {
         let node = Node(val: val)
-        
-        
         
         var beforeNode:Node? = self.foot
         if self.midd != nil {
@@ -53,16 +58,14 @@ class Solution_1670_DesignFrontMiddleBackQueue {
         }
         
         self.insert(node, before: beforeNode)
-        self.count = self.count + 1
-        self.updateMidd()
+        self.updateMidd(type: .push, position: .middle)
     }
 
     func pushBack(_ val: Int) {
         let node = Node(val: val)
         
         self.insert(node, before: self.foot)
-        self.count = self.count + 1
-        self.updateMidd()
+        self.updateMidd(type: .push, position: .back)
     }
 
     func popFront() -> Int {
@@ -75,8 +78,7 @@ class Solution_1670_DesignFrontMiddleBackQueue {
         }
         
         self.delete(node)
-        self.count = self.count - 1
-        self.updateMidd()
+        self.updateMidd(type: .pop, position: .front)
         return node.val
     }
     
@@ -90,8 +92,7 @@ class Solution_1670_DesignFrontMiddleBackQueue {
         }
         
         self.delete(node)
-        self.count = self.count - 1
-        self.updateMidd()
+        self.updateMidd(type: .pop, position: .middle)
         return node.val
     }
 
@@ -104,8 +105,7 @@ class Solution_1670_DesignFrontMiddleBackQueue {
         }
         
         self.delete(node)
-        self.count = self.count - 1
-        self.updateMidd()
+        self.updateMidd(type: .pop, position: .back)
         return node.val
     }
     
@@ -119,6 +119,8 @@ class Solution_1670_DesignFrontMiddleBackQueue {
         
         preNode.next = node
         nextNode.pre = node
+        self.count = self.count + 1
+        
     }
     
     private func delete(_ node: Node) {
@@ -127,33 +129,43 @@ class Solution_1670_DesignFrontMiddleBackQueue {
         
         preNode?.next = nextNode
         nextNode?.pre = preNode
+        
+        self.count = self.count - 1
     }
     
-    private func updateMidd(){
+    private func updateMidd(type:ChangeType, position:ChangePosition){
         guard self.count > 0 else {
             self.midd = nil
-            self.mid_position = -1
             return
         }
-        // self.count = 6            = 0      3   mid_tobe = 3
-        // self.count = 5           != 0                               3
         
-        // 6363  4                                 1
-        // 663
-        let mid_tobe = self.count % 2 == 0 ? self.count / 2  - 1:  self.count / 2
-        if mid_tobe == 0 {
+        guard self.count > 1 else {
             self.midd = self.head.next
-        }else {
-            self.midd = self.head
-            for _ in 0...mid_tobe {
-                self.midd = self.midd?.next
-            }
-//            if mid_tobe - self.mid_position == 1 {
-//                self.midd = self.midd?.next
-//            }else if mid_tobe == -1 {
-//                self.midd = self.midd?.pre
-//            }
+            return
         }
-        self.mid_position = mid_tobe
+        
+        if count % 2 == 0 {
+            // From odd to even, in most situation, we just need to move the midd point to it's previous node once, in other words,call midd?.pre
+            // such as [6,3,6] to [6,2,3,6] by pushMiddle, midd changed from node 3 to node 2, node 2 is the previous node of ndoe 3
+            
+            // But there are exceptions. They are popFront and pushBack
+            if (type == .pop && position == .front) || (type == .push && position == .back) {
+                // For example, [6,3,4,5,7] to [3,4,5,7] by popFront, or [6,3,4,5,7] to [6,3,4,5,7,3] by pushBack.
+                // As you can see, the midd point needn't change. so we need to be aware of them, and return them.
+                return
+            }
+            self.midd = self.midd?.pre
+        } else {
+            // Similar as from odd to even. when we meet the situation of from even to odd. We just need to move the midd point to it's next node once
+            // such as [6,2,3,4] to [6,2,3,4,5] by pushBack, midd changed from node 2 to node 3, node 3 is the next node of node 2
+            
+            // But there are also have exceptions. They are popBack and pushFront
+            if (type == .pop && position == .back) || (type == .push && position == .front) {
+                // For example, [6,2,3,4] to [6,2,3] by popBack, or [6,2,3,4] to [1,6,2,3,4] by pushFront.
+                // We can see, the midd point needn't change.
+                return
+            }
+            self.midd = self.midd?.next
+        }
     }
 }
