c8622c0aa2a7d558aabc3ca4be6305f50b400391
zixun <chenyl.exe@gmail.com>
2024-10-16 01:35:43 +0800

    add mergesort in 912

--- LeetCode-Swift/LeetCode-Swift/912_SortAnArray.swift
+++ LeetCode-Swift/LeetCode-Swift/912_SortAnArray.swift
@@ -9,11 +9,61 @@ class Solution_912_SortAnArray {
     
     func sortArray(_ nums: [Int]) -> [Int] {
         
-        return quickSort(nums)
+//        return quickSort(nums)
+        return mergeSort(nums)
     }
     
 }
 
+/// Merge Sort
+extension Solution_912_SortAnArray {
+    
+    private func mergeSort(_ nums: [Int]) -> [Int] {
+        
+        guard nums.count > 1 else {
+            return nums
+        }
+        
+        let mid = nums.count / 2
+        let left = Array(nums[0..<mid])
+        let right = Array(nums[mid...])
+        return merge(mergeSort(left), mergeSort(right))
+    }
+    
+    private func merge(_ left:[Int], _ right:[Int]) -> [Int] {
+        
+        var i = 0, j = 0
+        
+        var result = [Int]()
+        while i < left.count && j < right.count {
+            
+            if left[i] < right[j] {
+                result.append(left[i])
+                i = i + 1
+            }else {
+                result.append(right[j])
+                j = j + 1
+            }
+        }
+        
+        while i < left.count {
+            result.append(left[i])
+            i = i + 1
+        }
+        
+        while j < right.count {
+            result.append(right[j])
+            j = j + 1
+        }
+        
+        return result
+        
+    }
+    
+}
+
+
+/// Quick Sort
 extension Solution_912_SortAnArray {
     
     private func quickSort(_ nums: [Int]) -> [Int] {
