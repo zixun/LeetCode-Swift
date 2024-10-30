00e17ce13a0145ee19ff618c4e0c67f97c948c8a
zixun <chenyl.exe@gmail.com>
2024-10-30 22:20:58 +0800

    add 856

---
+++ LeetCode-Swift/LeetCode-Swift/856_ScoreofParentheses.swift
@@ -0,0 +1,14 @@
+//
+//  856_ScoreofParentheses.swift
+//  LeetCode-Swift
+//
+//  Created by zixun on 2024/10/30.
+//
+
+class Solution_856_ScoreofParentheses {
+    
+    func scoreOfParentheses(_ s: String) -> Int {
+            
+        
+    }
+}
