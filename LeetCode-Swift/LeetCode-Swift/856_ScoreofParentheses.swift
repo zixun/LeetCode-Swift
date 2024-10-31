26de595c622c70f9135748b2273193a0936db266
zixun <chenyl.exe@gmail.com>
2024-10-31 23:44:38 +0800

    add

--- LeetCode-Swift/LeetCode-Swift/856_ScoreofParentheses.swift
+++ LeetCode-Swift/LeetCode-Swift/856_ScoreofParentheses.swift
@@ -5,6 +5,14 @@
 //  Created by zixun on 2024/10/30.
 //
 
+//    Given a balanced parentheses string s, return the score of the string.
+//
+//    The score of a balanced parentheses string is based on the following rule:
+//
+//    "()" has score 1.
+//    AB has score A + B, where A and B are balanced parentheses strings.
+//    (A) has score 2 * A, where A is a balanced parentheses string.
+
 class Solution_856_ScoreofParentheses {
     
     func scoreOfParentheses(_ s: String) -> Int {
